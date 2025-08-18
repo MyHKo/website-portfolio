0ffd04c0d232301c44ca4b4d3d1ead54d43bfd74
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-08-18 10:04:24 +0200

    Hero-section: add paralax

--- src/Sections/Hero/index.tsx
+++ src/Sections/Hero/index.tsx
@@ -1,11 +1,28 @@
-import styles from './hero.module.less'
+import type { ReactElement } from "react";
+import {useSpring, animated} from "react-spring";
+import {to} from "@react-spring/web";
+import { useMove } from "react-use-gesture";
+import styles from "./hero.module.less";
 
-function HeroSection() {
+function HeroSection() :ReactElement {
+    const [style, api] = useSpring(() => ({x: 0,y: 0}));
+
+    const bind= useMove(({ xy: [px,py]}) => {
+        const x: number = (px / window.innerWidth - 0.5) * 100;
+        const y: number = (py / window.innerHeight - 0.5) * 100;
+        api.start({x, y});
+    })
 
     return (
-        <section className={styles.gradient}>
-            <div className={styles.half}></div>
-            <h1 className={styles.text}>Hello</h1>
+        <section {...bind()} className={styles.section}>
+            <animated.div className={styles.half} style={{
+                transform: to([style.x, style.y], (x, y) => `translate(${x}px, ${y}px)`)
+            }}></animated.div>
+            <animated.div className={styles.text} style={{
+                transform: to([style.x, style.y], (x, y) => `translate(${x/15}px, ${y/15}px)`)
+            }}>
+                Welcome.
+            </animated.div>
         </section>
     )
 }
