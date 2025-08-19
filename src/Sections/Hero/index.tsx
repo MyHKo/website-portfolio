ce4fc04d02871737d5bdc4f247e63e7fd4084811
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-08-19 21:01:42 +0200

    Hero-section: add the background component to the hero section

--- src/Sections/Hero/index.tsx
+++ src/Sections/Hero/index.tsx
@@ -3,8 +3,9 @@ import {useSpring, animated} from "react-spring";
 import {to} from "@react-spring/web";
 import { useMove } from "react-use-gesture";
 import styles from "./hero.module.less";
+import ParallaxBackground from "../ParallaxBackground";
 
-function HeroSection() :ReactElement {
+function HeroSection(): ReactElement {
     const [style, api] = useSpring(() => ({x: 0,y: 0}));
 
     const bind= useMove(({ xy: [px,py]}) => {
@@ -16,8 +17,10 @@ function HeroSection() :ReactElement {
     return (
         <section {...bind()} className={styles.section}>
             <animated.div className={styles.half} style={{
-                transform: to([style.x, style.y], (x, y) => `translate(${x}px, ${y}px)`)
-            }}></animated.div>
+                transform: to([style.x, style.y], (x, y) => `translate(${x/2}px, ${y/2}px)`)
+            }}>
+                <ParallaxBackground />
+            </animated.div>
             <animated.div className={styles.text} style={{
                 transform: to([style.x, style.y], (x, y) => `translate(${x/15}px, ${y/15}px)`)
             }}>
