16bcb741a46d2a12beea60aa9640c552a6366dd7
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-10-10 20:15:32 +0200

    Add creator for the pop particles

---
+++ src/Utils/generatePopParticleData.ts
@@ -0,0 +1,19 @@
+import type {PopParticle} from "../Interfaces/PopParticle.ts";
+import randomBetween from "./randomBetween.ts";
+
+function randomDirection(): {speedX: number; speedY: number} {
+    const theta: number = Math.random() * 2 * Math.PI;
+    return { speedX: Math.cos(theta)/2.3, speedY: Math.sin(theta)/2.3 };
+}
+
+function generatePopParticleData(x: number, y: number): PopParticle {
+    return {
+        x: x,
+        y: y,
+        colour: `rgba(${randomBetween([0,255])}, ${randomBetween([0,255])}, ${randomBetween([0,255])}, 1)`,
+        age: 1,
+        ...randomDirection()
+    }
+}
+
+export default generatePopParticleData;
