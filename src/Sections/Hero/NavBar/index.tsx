155029fa963936a64724f9deda07779b9e1c9e3f
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-10-20 19:06:41 +0200

    Add navbar

---
+++ src/Sections/Hero/NavBar/index.tsx
@@ -0,0 +1,24 @@
+import styles from "./navbar.module.less"
+
+function Navbar() {
+
+    return (
+        <nav className={}>
+            ::before
+            <ul>
+                <li>
+                    <a href="/"></a>
+                </li>
+                <li>
+                    <a href="/"></a>
+                </li>
+                <li>
+                    <a href="/"></a>
+                </li>
+            </ul>
+            ::after
+        </nav>
+    )
+}
+
+export default Navbar;
