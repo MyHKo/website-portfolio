9b3842f3cda8e6cb2543a6c2e3b767b8f2b924ee
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-11-07 09:27:37 +0100

    Re-paint circles on the static background

--- src/backgrounds/StaticBackground/index.tsx
+++ src/backgrounds/StaticBackground/index.tsx
@@ -2,12 +2,14 @@ import {type ReactElement} from "react";
 import styles from "./staticBackground.module.less";
 
 function StaticBackground(): ReactElement {
+    const secondaryColor = "#006BA6";
     const circles = Array.from({ length: 25 }, (_, i) => ({
         id: i,
         size: Math.random() * 200 + 40,
         left: Math.random() * 100,
         top: Math.random() * 100,
         opacity: Math.random() * 0.25 + 0.1,
+        color: Math.random() < 0.4 ? secondaryColor : "#888",
     }));
 
     return (
@@ -22,6 +24,7 @@ function StaticBackground(): ReactElement {
                         left: `${circle.left}%`,
                         top: `${circle.top}%`,
                         opacity: circle.opacity,
+                        backgroundColor: circle.color,
                     }}
                 />
             ))}
