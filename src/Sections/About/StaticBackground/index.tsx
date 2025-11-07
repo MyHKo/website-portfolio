9758884058ca15dbbe9b5be8953949d8ad66fc18
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-11-07 08:37:29 +0100

    Change background of the about section

--- src/Sections/About/StaticBackground/index.tsx
+++ src/Sections/About/StaticBackground/index.tsx
@@ -2,13 +2,13 @@ import {type ReactElement} from "react";
 import styles from "./staticBackground.module.less";
 
 function StaticBackground(): ReactElement {
-    // Generate static grey circles of different sizes
-    const circles = Array.from({ length: 15 }, (_, i) => ({
+    // Generate static grey circles of different sizes scattered across the background
+    const circles = Array.from({ length: 25 }, (_, i) => ({
         id: i,
-        size: Math.random() * 100 + 30, // Random size between 30 and 130px
+        size: Math.random() * 200 + 40, // Random size between 40 and 240px (more variety)
         left: Math.random() * 100, // Random horizontal position (percentage)
         top: Math.random() * 100, // Random vertical position (percentage)
-        opacity: Math.random() * 0.3 + 0.1, // Random opacity between 0.1 and 0.4
+        opacity: Math.random() * 0.25 + 0.1, // Random opacity between 0.1 and 0.35
     }));
 
     return (
