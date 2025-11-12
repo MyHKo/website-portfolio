cf9b92df9895a0d3b5c3e7f706b2b7f478268f32
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-11-12 19:46:00 +0100

    Add Icon link

---
+++ src/Sections/Contact/IconLink/IconLink.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './IconLink.module.less';
+
+interface IconLinkProps {
+    icon: React.ReactNode;
+    url: string;
+}
+
+const IconLink: React.FC<IconLinkProps> = ({ icon, url }) => {
+    const handleClick = () => {
+        window.open(url, '_blank'); // open in a new tab
+    };
+
+    return (
+        <div className={styles.iconWrapper} onClick={handleClick}>
+            <div className={styles.icon}></div>
+            {icon}
+        </div>
+);
+};
+
+export default IconLink;
